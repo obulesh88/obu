97fc50be51a511e96aa40f74eea2614209c51107
Obuleswar <obuleswar88@gmail.com>
2026-01-26 02:52:55 +0000

    Set this code to cube merge  <iframe   src="https://html5.gamemonetize.c

--- src/app/earning/games/page.tsx
+++ src/app/earning/games/page.tsx
@@ -22,7 +22,7 @@ const games = [
     { name: "My Cat Restaurant", url: "https://html5.gamemonetize.co/zro5d0oom4aubos4mlka610s5mla0zt3/" },
     { name: "Memory Emoji", url: "https://html5.gamemonetize.co/3s56bhfz1vc2njsrlqzgnfs9m2r9eabf/" },
     { name: "Gear Shift Race", url: "https://html5.gamemonetize.co/bwxvpns0l9v0vxqcsj2v5uw6ummx54zr/" },
-    { name: "Cube Merge", url: "https://html5.gamemonetize.co/lnp25m8d5j5e7f0n3p4g6g2f0g0g5g/" },
+    { name: "Cube Merge", url: "https://html5.gamemonetize.co/nkjtibnj0tezzvci3owaf2dtff3hvfvq/" },
     { name: "Game #8", url: "https://html5.gamemonetize.co/lnp25m8d5j5e7f0n3p4g6g2f0g0g5g/" },
 ];
 
@@ -118,7 +118,7 @@ export default function GamesPage() {
         return newTimers;
     });
 
-    if (games[index].name === 'My Cat Restaurant' || games[index].name === "Line Color Puzzle" || games[index].name === "Bubble Shooter Relaxing Puzzle" || games[index].name === "Count Rush" || games[index].name === "Memory Emoji" || games[index].name === "Gear Shift Race") {
+    if (games[index].name === 'My Cat Restaurant' || games[index].name === "Line Color Puzzle" || games[index].name === "Bubble Shooter Relaxing Puzzle" || games[index].name === "Count Rush" || games[index].name === "Memory Emoji" || games[index].name === "Gear Shift Race" || games[index].name === "Cube Merge") {
       setSelectedGame({ game: games[index], index });
     } else {
       window.open(games[index].url, '_blank');
