dd7dcb18cc73645e6d42e40cfde8e2ad972c0360
Obuleswar <obuleswar88@gmail.com>
2026-02-25 09:31:18 +0000

    Set this configurations // For Firebase JS SDK v7.20.0 and later, measur

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyBAOO7Gn1Y64dx_imZK0v_q_HaRzc6UYqc",
-  authDomain: "earning-app-ff02b.firebaseapp.com",
-  databaseURL: "https://earning-app-ff02b-default-rtdb.firebaseio.com",
-  projectId: "earning-app-ff02b",
-  storageBucket: "earning-app-ff02b.appspot.com",
-  messagingSenderId: "195041352341",
-  appId: "1:195041352341:web:517d304706a851dcc7999c",
-  measurementId: "G-FFVVEGQRFH"
+  apiKey: "AIzaSyDarIxcEt5SCi7pfL9JY0S7857dgRsVU00",
+  authDomain: "wallet-8fdd4.firebaseapp.com",
+  databaseURL: "https://wallet-8fdd4-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wallet-8fdd4",
+  storageBucket: "wallet-8fdd4.firebasestorage.app",
+  messagingSenderId: "622824988525",
+  appId: "1:622824988525:web:71dcc82dab96e7f963c464",
+  measurementId: "G-EY3J3RTQDR"
 };
