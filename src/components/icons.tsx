e29724786e3171fe9e7e5e2fe3ef6d31abecd781
Obuleswar <obuleswar88@gmail.com>
2026-03-14 16:11:57 +0000

    In that header logo set this logic

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,12 +4,34 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
+      viewBox="0 0 100 100"
       fill="none"
       {...props}
     >
-      <path d="M12 2L2 22H22L12 2Z" fill="hsl(var(--primary))" />
-      <path d="M2 22L12 12L22 22H2Z" fill="hsl(var(--foreground))" />
+      {/* Outer Glow/Background Stylization */}
+      <circle cx="50" cy="50" r="45" fill="currentColor" fillOpacity="0.05" />
+      
+      {/* O Character */}
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M42 28H18C12.4772 28 8 32.4772 8 38V62C8 67.5228 12.4772 72 18 72H42C47.5228 72 52 67.5228 52 62V38C52 32.4772 47.5228 28 42 28ZM38 58H22V42H38V58Z"
+        fill="hsl(var(--primary))"
+      />
+      
+      {/* R Character */}
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M58 28V72H72V54H82C88.6274 54 94 48.6274 94 42C94 35.3726 88.6274 30 82 30L58 28ZM72 44V36H82C85.3137 36 88 38.6863 88 42C88 45.3137 85.3137 48 82 48H72V44Z"
+        fill="hsl(var(--primary))"
+      />
+      
+      {/* R Leg */}
+      <path
+        d="M82 54L96 72H84L72 54H82Z"
+        fill="hsl(var(--primary))"
+      />
     </svg>
   );
 }
